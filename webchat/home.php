39b87a4eec9eb4f2c1938c00ba0584cd12a67b52
savinduekanayake <47809365+savinduekanayake@users.noreply.github.com>
2019-02-20 12:58:27 +0530

    Add files via upload

---
+++ webchat/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Home</title>
+</head>
+
+<body>
+<div id="main">
+    
+<h1 style="backgroun-color : #6495ed; color: white; ">-online</h1>
+    
+    <div class="output">
+    
+    </div>
+    
+</div>    
+    
+</body>
+
+
+</html>
